3f811c0d9c6b7ca72b2a8afbfe4e37554595e4bf
Ilia Baryshnikov <qwelias@gmail.com>
2018-05-17 13:51:23 +0300

    fix throw in return statement

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-module.exports = makeGenerator => function () {
-	const generator = makeGenerator.apply(this, arguments)
-	const handle = result => {
-		if (result.done) return Promise.resolve(result.value)
-		return Promise.resolve(result.value).then(res => {
-			return handle(generator.next(res))
-		}, err => {
-			return handle(generator.throw(err))
-		})
-	}
-	try {
-		return handle(generator.next())
-	} catch (ex) {
-		return Promise.reject(ex)
+module.exports = makeGenerator => {
+	return function () {
+		const generator = makeGenerator.apply(this, arguments)
+		const handle = result => {
+			return Promise.resolve(result.value).then(res => {
+                if (result.done) return res;
+				return handle(generator.next(res))
+			}, err => {
+				return handle(generator.throw(err))
+			})
+		}
+		try {
+			return handle(generator.next())
+		} catch (ex) {
+			return Promise.reject(ex)
+		}
 	}
 }
